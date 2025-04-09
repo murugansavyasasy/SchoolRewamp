ca6d3888841d2824c9dab6b212d5a1a0cee2457b
murugansavyasasy <murugan@savyasasy.com>
2025-04-09 12:48:47 +0530

    push

--- app/src/main/java/com/vs/schoolmessenger/AWS/AWSKeys.kt
+++ app/src/main/java/com/vs/schoolmessenger/AWS/AWSKeys.kt
@@ -1,5 +1,5 @@
-//package com.vs.schoolmessenger.AWS
-//
-//object AWSKeys {
-//    const val BUCKET_NAME: String = "gradit-communication"
-//}
+package com.vs.schoolmessenger.AWS
+
+object AWSKeys {
+    const val BUCKET_NAME: String = "gradit-communication"
+}
