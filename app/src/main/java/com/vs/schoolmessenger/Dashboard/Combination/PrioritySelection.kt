54aebe47a70566dfa540ff3e7b9f3709ad12a2c9
murugansavyasasy <murugan@savyasasy.com>
2025-04-18 10:04:29 +0530

    pull latest

--- app/src/main/java/com/vs/schoolmessenger/Dashboard/Combination/PrioritySelection.kt
+++ app/src/main/java/com/vs/schoolmessenger/Dashboard/Combination/PrioritySelection.kt
@@ -162,11 +162,14 @@ class PrioritySelection : BaseActivity<RoleSelecionBinding>(), View.OnClickListe
                 }
                 isLoadData(true)
                 Constant.isParentChoose = false
-            }
-        }
 
-            isClickingId.background = ContextCompat.getDrawable(this, R.drawable.bg_blue)
-                    isClickingId.setTextColor(ContextCompat.getColor(this, R.color.white))
+            }
+
+
+        }
+        isClickingId.background = ContextCompat.getDrawable(this, R.drawable.bg_blue)
+        isClickingId.setTextColor(ContextCompat.getColor(this, R.color.white))
+
         }
 
         override fun onItemClick(data: ChildDetails) {
