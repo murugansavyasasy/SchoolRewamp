8fce46fff4b103514c45ddc8117745e2701563ef
murugansavyasasy <murugan@savyasasy.com>
2025-05-13 15:23:12 +0530

    notification added

--- app/src/main/java/com/vs/schoolmessenger/Dashboard/Parent/ParentDashboard.kt
+++ app/src/main/java/com/vs/schoolmessenger/Dashboard/Parent/ParentDashboard.kt
@@ -40,14 +40,15 @@ class ParentDashboard : BaseActivity<ChildDashboardBinding>(), View.OnClickListe
             R.id.customBottomNav
         )
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener {
-            if (!it.isSuccessful) {
-                return@addOnCompleteListener
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val token = task.result
+                    Log.d("FCM", "Token: $token")
+                    isUpdateDeviceToken(token)
+
+                }
             }
-            val token = it.result //this is the token retrieved
-            Log.d("isDeviceToken",token)
-            isUpdateDeviceToken(token)
-        }
 
         authViewModel!!.isDeviceToken?.observe(this) { response ->
             if (response != null) {
