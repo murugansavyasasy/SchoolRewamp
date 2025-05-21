cbd8cd080d5ecb7bc2f14363be8b7a4cebd79551
murugansavyasasy <murugan@savyasasy.com>
2025-05-21 17:05:53 +0530

    latest

--- app/src/main/java/com/vs/schoolmessenger/School/ImportantInfo/ImportantInfo.kt
+++ app/src/main/java/com/vs/schoolmessenger/School/ImportantInfo/ImportantInfo.kt
@@ -2,23 +2,34 @@ package com.vs.schoolmessenger.School.ImportantInfo
 
 import android.view.View
 import com.vs.schoolmessenger.Auth.Base.BaseActivity
-import com.vs.schoolmessenger.databinding.LeaveRequestsBinding
+import com.vs.schoolmessenger.R
+import com.vs.schoolmessenger.Utils.Constant
+import com.vs.schoolmessenger.databinding.ImportantInfoBinding
 
-class ImportantInfo : BaseActivity<LeaveRequestsBinding>(),
+class ImportantInfo : BaseActivity<ImportantInfoBinding>(),
     View.OnClickListener {
 
-    override fun getViewBinding(): LeaveRequestsBinding {
-        return LeaveRequestsBinding.inflate(layoutInflater)
+    override fun getViewBinding(): ImportantInfoBinding {
+        return ImportantInfoBinding.inflate(layoutInflater)
     }
 
     override fun setupViews() {
         super.setupViews()
         setupToolbar()
+        setUpGradientSchool()
+
+        Constant.loadWebView(
+            this,
+            binding.webView,
+            Constant.terms_condition
+        )
     }
 
-    override fun onClick(p0: View?) {
-        when (p0?.id) {
-
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.imgBack -> {
+                finish()
+            }
         }
     }
 }
