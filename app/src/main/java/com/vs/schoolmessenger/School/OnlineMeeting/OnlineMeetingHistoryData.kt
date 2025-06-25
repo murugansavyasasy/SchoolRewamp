43bb9d7f3a71cc14e208a32aa63c26b20d34e4aa
murugansavyasasy <murugan@savyasasy.com>
2025-06-25 14:33:43 +0530

    latest

--- app/src/main/java/com/vs/schoolmessenger/School/OnlineMeeting/OnlineMeetingHistoryData.kt
+++
@@ -1,9 +0,0 @@
-package com.vs.schoolmessenger.School.OnlineMeeting
-
-data class OnlineMeetingHistoryData(
-    val title: String,
-    val time: String,
-    val date: String,
-    val meetingType: String,
-    val link: String
-)
