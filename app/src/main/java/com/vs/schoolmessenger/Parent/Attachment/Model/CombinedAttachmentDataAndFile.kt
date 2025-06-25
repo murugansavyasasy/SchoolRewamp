3b3eb9790db1198f96b6ff3870caf83019ee14dc
murugansavyasasy <murugan@savyasasy.com>
2025-06-25 18:11:01 +0530

    sathish

--- app/src/main/java/com/vs/schoolmessenger/Parent/Attachment/Model/CombinedAttachmentDataAndFile.kt
+++
@@ -1,7 +0,0 @@
-package com.vs.schoolmessenger.Parent.Attachment.Model
-
-class CombinedAttachmentDataAndFile (
-    val attachmentFile: AttachmentFile,
-    val attachmentData: AttachmentData
-
-    )
