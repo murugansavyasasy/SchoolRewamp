c577ec8b5b82294bd0d87f5ef08ae04ebebd8903
murugansavyasasy <murugan@savyasasy.com>
2025-06-30 12:41:20 +0530

    Pull By saran

--- app/src/main/java/com/vs/schoolmessenger/Parent/Coupon/CouponDashboardView.kt
+++ app/src/main/java/com/vs/schoolmessenger/Parent/Coupon/CouponDashboardView.kt
@@ -1,4 +1,25 @@
 package com.vs.schoolmessenger.Parent.Coupon
 
-class CouponDashboardView {
+import android.view.View
+import com.vs.schoolmessenger.Auth.Base.BaseActivity
+import com.vs.schoolmessenger.databinding.CouponDashboardBinding
+
+class CouponDashboardView : BaseActivity<CouponDashboardBinding>(), View.OnClickListener {
+
+    override fun getViewBinding(): CouponDashboardBinding {
+        return CouponDashboardBinding.inflate(layoutInflater)
+    }
+
+    override fun setupViews() {
+        super.setupViews()
+        setUpGradientParent()
+
+    }
+
+
+
+    override fun onClick(p0: View?) {
+
+    }
+
 }
