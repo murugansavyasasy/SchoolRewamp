741084470b796010ff36a39db0c3d68b75d6ec56
murugansavyasasy <murugan@savyasasy.com>
2025-10-04 11:26:47 +0530

    pull by nicholes

--- app/src/main/java/com/vs/schoolmessenger/School/Communication/CommunicationSchool.kt
+++ app/src/main/java/com/vs/schoolmessenger/School/Communication/CommunicationSchool.kt
@@ -758,7 +758,7 @@ class CommunicationSchool : BaseActivity<CommunicationSchoolBinding>(), View.OnC
                 isScheduleCall = false
                 Constant.isCommunicationType = 3
                 selectedDates.clear()
-//                selectedDatesAdapter?.submitSelectedDates(emptyList())
+                selectedDatesAdapter?.submitSelectedDates(emptyList())
                 if (mAdapter != null) {
                     mAdapter!!.releaseMediaPlayer()
                 }
