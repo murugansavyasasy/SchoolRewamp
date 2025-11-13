332835822b86208255ba89283358192240145252
murugansavyasasy <murugan@savyasasy.com>
2025-11-13 18:54:54 +0530

    Pull By saran

--- app/src/main/java/com/vs/schoolmessenger/Utils/Constant.kt
+++ app/src/main/java/com/vs/schoolmessenger/Utils/Constant.kt
@@ -2598,7 +2598,7 @@ object Constant {
     fun convertDateTimeFormat(input: String): String {
         return try {
             val inputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-            val outputFormat = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
             val date = inputFormat.parse(input)
             outputFormat.format(date!!)
         } catch (e: Exception) {
