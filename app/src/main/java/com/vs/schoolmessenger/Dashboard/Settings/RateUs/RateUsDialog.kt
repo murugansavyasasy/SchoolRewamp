b84bb4512f59cccd59051f2118289ffdaa737cf7
murugansavyasasy <murugan@savyasasy.com>
2025-12-05 18:00:17 +0530

    Pull By Saran

--- app/src/main/java/com/vs/schoolmessenger/Dashboard/Settings/RateUs/RateUsDialog.kt
+++ app/src/main/java/com/vs/schoolmessenger/Dashboard/Settings/RateUs/RateUsDialog.kt
@@ -130,7 +130,6 @@ class RateUsDialog(   private val fromScreen: String?,
     private fun bindCategoryList(list: List<CategoryItem>) {
         val recycler = binding.recyclerCategories
         recycler.visibility = View.VISIBLE
-        binding.recyclerScroll.visibility = View.VISIBLE
 
         val layoutManager = FlexboxLayoutManager(requireContext()).apply {
             flexDirection = FlexDirection.ROW
@@ -148,7 +147,6 @@ class RateUsDialog(   private val fromScreen: String?,
         lblMayBeLater.visibility = View.GONE
         lnrRatingContent.visibility = View.VISIBLE
         recyclerCategories.visibility = View.VISIBLE
-        binding.recyclerScroll.visibility = View.VISIBLE
     }
 
     private fun showMaybeLater() = with(binding) {
@@ -156,7 +154,6 @@ class RateUsDialog(   private val fromScreen: String?,
         lblMayBeLater.visibility = View.VISIBLE
         lnrRatingContent.visibility = View.GONE
         recyclerCategories.visibility = View.GONE
-        binding.recyclerScroll.visibility = View.GONE
     }
 
     override fun onClick(v: View?) {
