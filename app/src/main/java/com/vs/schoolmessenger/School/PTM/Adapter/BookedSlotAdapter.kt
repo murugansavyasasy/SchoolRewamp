e9cef4fb25bc7dd168a344cdb76db1269cbfaada
murugansavyasasy <murugan@savyasasy.com>
2025-12-09 10:11:57 +0530

    sathish

--- app/src/main/java/com/vs/schoolmessenger/School/PTM/Adapter/BookedSlotAdapter.kt
+++ app/src/main/java/com/vs/schoolmessenger/School/PTM/Adapter/BookedSlotAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -73,6 +75,11 @@ class BookedSlotAdapter(
 
         private val txtMotherName: TextView = itemView.findViewById(R.id.txtMotherName)
         private val txtFatherName: TextView = itemView.findViewById(R.id.txtFatherName)
+        private val lblStatus: TextView = itemView.findViewById(R.id.lblStatus)
+        private val imgCheckmark: ImageView = itemView.findViewById(R.id.imgCheckmark)
+        private val rytStatusBanner: RelativeLayout = itemView.findViewById(R.id.rytStatusBanner)
+        private val lblWaitingBooking: TextView = itemView.findViewById(R.id.lblWaitingBooking)
+        private val cardBookedBy: CardView = itemView.findViewById(R.id.cardBookedBy)
 
         fun bind(data: BookedSlotItem) {
             lblTitle.text = data.event_name ?: ""
@@ -100,6 +107,73 @@ class BookedSlotAdapter(
                 notifyDataSetChanged()
             }
 
+            if (data.can_cancel!!){
+                if (data.slot_status=="Completed"){
+                    imgDot.visibility= View.GONE
+                }else{
+                    imgDot.visibility= View.VISIBLE
+                }
+            }else{
+                imgDot.visibility= View.GONE
+            }
+            if (data.is_cancelled_by_staff!!) {
+                lblWaitingBooking.visibility = View.VISIBLE
+                rytStatusBanner.background = context.getDrawable(R.drawable.bg_light_red_radious)
+                cardBookedBy.visibility = View.GONE
+                lblStatus.setTextColor(context.getColor(R.color.red))
+                lblStatus.text = "Canceled"
+                lblWaitingBooking.setTextColor(context.getColor(R.color.red))
+                lblWaitingBooking.text = "Slot Canceled"
+                imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.cancelled))
+                lblWaitingBooking.background = context.getDrawable(R.drawable.bg_light_red_radious)
+            } else {
+                if (data.slot_status == "Available") {
+                    lblWaitingBooking.visibility = View.GONE
+                    cardBookedBy.visibility = View.VISIBLE
+                    lblStatus.setTextColor(context.getColor(R.color.Blue))
+                    rytStatusBanner.background = context.getDrawable(R.drawable.rect_bg_light_green_present)
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.checkmark_circle))
+
+                } else if (data.slot_status == "Cancelled") {
+                    lblWaitingBooking.visibility = View.VISIBLE
+                    cardBookedBy.visibility = View.GONE
+                    rytStatusBanner.background = context.getDrawable(R.drawable.bg_light_red_radious)
+                    lblWaitingBooking.text = "Slot Cancelled"
+                    lblStatus.setTextColor(context.getColor(R.color.red))
+                    lblWaitingBooking.setTextColor(context.getColor(R.color.red))
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.cancelled))
+                    lblWaitingBooking.background = context.getDrawable(R.drawable.bg_light_red_radious)
+                } else if (data.slot_status == "Expired") {
+                    lblWaitingBooking.visibility = View.VISIBLE
+                    cardBookedBy.visibility = View.GONE
+                    rytStatusBanner.background = context.getDrawable(R.drawable.gray_bg_radius)
+                    lblWaitingBooking.text = "Slot Expired"
+                    lblWaitingBooking.setTextColor(context.getColor(R.color.black))
+                    lblStatus.setTextColor(context.getColor(R.color.gnt_gray))
+                    lblWaitingBooking.background = context.getDrawable(R.drawable.gray_bg_radius)
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.expired))
+                } else if (data.slot_status == "Completed") {
+                    lblWaitingBooking.visibility = View.GONE
+                    rytStatusBanner.background = context.getDrawable(R.drawable.rect_bg_light_green_present)
+                    cardBookedBy.visibility = View.VISIBLE
+                    lblStatus.setTextColor(context.getColor(R.color.green))
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.checkmark_circle))
+                } else if (data.slot_status == "Booked") {
+                    rytStatusBanner.background = context.getDrawable(R.drawable.rect_bg_light_green_present)
+                    lblWaitingBooking.visibility = View.GONE
+                    cardBookedBy.visibility = View.VISIBLE
+                    lblStatus.setTextColor(context.getColor(R.color.green))
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.checkmark_circle))
+                } else if (data.slot_status == "Upcoming") {
+                    lblWaitingBooking.visibility = View.GONE
+                    rytStatusBanner.background = context.getDrawable(R.drawable.rect_bg_light_green_present)
+                    cardBookedBy.visibility = View.VISIBLE
+                    lblStatus.setTextColor(context.getColor(R.color.green))
+                    lblStatus.text = "Booked"
+                    imgCheckmark.setImageDrawable(context.getDrawable(R.drawable.checkmark_circle))
+                }
+            }
+
             imgDot.setOnClickListener {
                 listener.onBookedSlotCancelReOpenClickListener(
                     data,
@@ -114,6 +188,5 @@ class BookedSlotAdapter(
                 .error(R.drawable.profile)
                 .into(imgProfile)
         }
-
     }
 }
