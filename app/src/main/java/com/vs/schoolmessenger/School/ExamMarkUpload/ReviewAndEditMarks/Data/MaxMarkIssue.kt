b531047fcf0374ae53033ffbfe6c4516e1b354ea
murugansavyasasy <murugan@savyasasy.com>
2026-01-03 13:27:14 +0530

    sathish

--- app/src/main/java/com/vs/schoolmessenger/School/ExamMarkUpload/ReviewAndEditMarks/Data/MaxMarkIssue.kt
+++ app/src/main/java/com/vs/schoolmessenger/School/ExamMarkUpload/ReviewAndEditMarks/Data/MaxMarkIssue.kt
@@ -3,6 +3,7 @@ package com.vs.schoolmessenger.School.ExamMarkUpload.ReviewAndEditMarks.Data
 data class MaxMarkIssue(
     val studentName: String,
     val subjectName: String,
+    val activityName: String,
     val enteredMark: String,
     val maxMark: Int
 )
