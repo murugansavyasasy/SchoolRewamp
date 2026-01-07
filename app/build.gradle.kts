cabd835d9308051c83ef10d3d4f1d2857cfc60f2
murugansavyasasy <murugan@savyasasy.com>
2026-01-07 18:32:35 +0530

    Pull By saran

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,13 +47,15 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
-
     }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
