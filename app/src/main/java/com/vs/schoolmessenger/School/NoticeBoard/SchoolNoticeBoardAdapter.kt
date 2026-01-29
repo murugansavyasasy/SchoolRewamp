f235863c27144d0696b9767dad033e39ac70b1a7
murugansavyasasy <murugan@savyasasy.com>
2026-01-29 18:50:18 +0530

    by muru

--- app/src/main/java/com/vs/schoolmessenger/School/NoticeBoard/SchoolNoticeBoardAdapter.kt
+++ app/src/main/java/com/vs/schoolmessenger/School/NoticeBoard/SchoolNoticeBoardAdapter.kt
@@ -336,17 +336,17 @@ class SchoolNoticeBoardAdapter(
         private fun scheduleNotification(context: Context, triggerTime: Long) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                if (!alarmManager.canScheduleExactAlarms()) {
-                    val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
-                        data = Uri.parse("package:${context.packageName}")
-                    }
-                    context.startActivity(intent)
-                    Toast.makeText(
-                        context,
-                        context.getString(R.string.Please_allow_exact_alarm_permission),
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+//                if (!alarmManager.canScheduleExactAlarms()) {
+//                    val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+//                        data = Uri.parse("package:${context.packageName}")
+//                    }
+//                    context.startActivity(intent)
+//                    Toast.makeText(
+//                        context,
+//                        context.getString(R.string.Please_allow_exact_alarm_permission),
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
             }
         }
     }
