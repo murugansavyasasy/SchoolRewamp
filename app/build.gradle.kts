ba07d4b1a8f9577b72a7d2c57a7a36448dd96bbe
murugansavyasasy <murugan@savyasasy.com>
2026-01-30 12:09:58 +0530

    by sathi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 24
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 171
+        versionCode = 170
         versionName = "8.0"
         // 👇 Add these lines
         buildConfigField("int", "VERSION_CODE", versionCode.toString())
