d50dba4b0a329e38e447c565f203a2a2e19e99d6
murugansavyasasy <murugan@savyasasy.com>
2026-03-04 09:52:43 +0530

    pull by sathish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -291,7 +291,6 @@ dependencies {
     implementation("com.github.chrisbanes:PhotoView:2.3.0")
     implementation("androidx.lifecycle:lifecycle-process:2.10.0")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
-    implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("androidx.core:core-splashscreen:1.2.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
