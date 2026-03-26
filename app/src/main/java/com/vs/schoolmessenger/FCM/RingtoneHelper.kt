c646e6806d59f9d24b86a9ee8140f82d645bf5e6
murugansavyasasy <murugan@savyasasy.com>
2026-03-26 14:50:30 +0530

    push by sathish

--- app/src/main/java/com/vs/schoolmessenger/FCM/RingtoneHelper.kt
+++ app/src/main/java/com/vs/schoolmessenger/FCM/RingtoneHelper.kt
@@ -11,8 +11,8 @@ import com.vs.schoolmessenger.R
 
 object RingtoneHelper {
 
-    private var ringtone: Ringtone? = null
-    private var vibrator: Vibrator? = null
+//    private var ringtone: Ringtone? = null
+//    private var vibrator: Vibrator? = null
 
     var isPlaying = false
 
@@ -25,18 +25,18 @@ object RingtoneHelper {
             val uri =
                 Uri.parse("android.resource://${context.packageName}/${R.raw.call_notification}")
 
-            ringtone = RingtoneManager.getRingtone(context, uri)
-            ringtone?.play()
+//            ringtone = RingtoneManager.getRingtone(context, uri)
+//            ringtone?.play()
 
-            vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+//            vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator?.vibrate(
-                    VibrationEffect.createWaveform(longArrayOf(0, 500, 1000), 0)
-                )
-            } else {
-                vibrator?.vibrate(longArrayOf(0, 500, 1000), 0)
-            }
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                vibrator?.vibrate(
+//                    VibrationEffect.createWaveform(longArrayOf(0, 500, 1000), 0)
+//                )
+//            } else {
+//                vibrator?.vibrate(longArrayOf(0, 500, 1000), 0)
+//            }
             isPlaying = true
 
         } catch (e: Exception) {
@@ -45,9 +45,9 @@ object RingtoneHelper {
     }
 
     fun stop() {
-        ringtone?.stop()
-        ringtone = null
-        vibrator?.cancel()
+//        ringtone?.stop()
+//        ringtone = null
+//        vibrator?.cancel()
         isPlaying = false
     }
 }
