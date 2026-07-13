1811faeee326d7c59cd0fa79482b21be0003530a
murugansavyasasy <murugan@savyasasy.com>
2026-07-13 16:54:29 +0530

    by saran

--- app/src/main/java/com/vs/schoolmessenger/Dashboard/Parent/ChildMenuAdapter.kt
+++ app/src/main/java/com/vs/schoolmessenger/Dashboard/Parent/ChildMenuAdapter.kt
@@ -135,7 +135,7 @@ class ChildMenuAdapter(
 //                Constant.M_BUS_TRACKING -> imgMenu.setImageResource(R.drawable.ic_bus_green)
                 Constant.M_LIVE_BUS_TRACKING -> imgMenu.setImageResource(R.drawable.bus_tracking_icon)
                 Constant.M_STUDENTDATE -> imgMenu.setImageResource(R.drawable.student_report)
-                Constant.M_CLASSTEST -> imgMenu.setImageResource(R.drawable.student_report)
+                Constant.M_CLASSTEST -> imgMenu.setImageResource(R.drawable.classtestprofile)
             }
 
             rlaMenu.setOnClickListener {
