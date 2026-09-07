a6626fb1748b1bbe9796d9d8be2e351459beeb1e
murugansavyasasy <murugan@savyasasy.com>
2026-09-07 10:32:29 +0530

    App Language conversion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,19 @@ android {
             useLegacyPackaging = false
         }
     }
+
+    bundle {
+        language {
+            enableSplit = false
+        }
+    }
+
     defaultConfig {
         applicationId = "com.vs.schoolmessenger"
         minSdk = 24
         //noinspection EditedTargetSdkVersion
         targetSdk = 37
-        versionCode = 210
+        versionCode = 213
 
         versionName = "8.12"
         // 👇 Add these lines
